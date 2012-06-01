d0caf51c020efc7d39a3ef0f3a4c14f152e4c962
jmoswalt <jmoswalt@gmail.com>
2012-05-31 21:41:13 -0500

    Change file perms.

--- settings.py
+++ settings.py
@@ -149,6 +149,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'proman',
+    'gunicorn',
 )
 
 AUTH_PROFILE_MODULE = 'proman.Profile'
