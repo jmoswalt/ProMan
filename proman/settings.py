7cac60c586469e3267a9352f2e343137999333cf
jmoswalt <jmoswalt@gmail.com>
2012-07-18 20:30:40 -0500

    Updated caching to work with memcached servers

--- proman/settings.py
+++ proman/settings.py
@@ -175,6 +175,15 @@ CACHES = {
     }
 }
 
+MEMCACHE_SERVERS = env('MEMCACHE_SERVERS', '')
+
+if MEMCACHE_SERVERS:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django_pylibmc.memcached.PyLibMCCache'
+        }
+    }
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
