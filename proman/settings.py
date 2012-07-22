b84af7ac7234df65abd6b4ae7760f8ca0a8ceee3
jmoswalt <jmoswalt@gmail.com>
2012-07-22 15:23:24 -0500

    Cleaned up settings. Will now load .env file to environment

--- proman/settings.py
+++ proman/settings.py
@@ -2,21 +2,39 @@ import os
 from os import environ
 import dj_database_url
 
+PROJECT_ROOT = os.path.abspath('.')
+ROOT_URLCONF = 'proman.urls'
+WSGI_APPLICATION = 'proman.wsgi.application'
+
 # Helper lambda for gracefully degrading environmental variables:
 env = lambda e, d: environ[e] if environ.has_key(e) else d
 
-PROJECT_ROOT = os.path.abspath('.')
+# Load the .env file into the os.environ for secure information
+for line in open(os.path.join(PROJECT_ROOT, '.env'), 'r').readlines():
+    env_key = line.rstrip().split("=")[0]
+    if env_key:
+        # set the environment variable to the value with the start and
+        # end quotes taken off.
+        environ[env_key] = ''.join(line.rstrip().split("=")[1:])[1:-1]
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
 
-try:
-    import local
-except ImportError:
-    pass
+# -------------------------------------- #
+# DEBUG
+# -------------------------------------- #
+DEBUG = env('DEBUG', False)
+TEMPLATE_DEBUG = env('TEMPLATE_DEBUG', DEBUG)
 
+
+# -------------------------------------- #
+# DATABASES
+# -------------------------------------- #
+# Databases uses dj_database_url to pull DATABASE_URL from the environment
 DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
+
+# -------------------------------------- #
+# EMAIL
+# -------------------------------------- #
 EMAIL_HOST = env('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = env('EMAIL_PORT', 587)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', 'your_email@example.com')
@@ -24,13 +42,10 @@ EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+
+# -------------------------------------- #
+# TIME AND LANGUAGE
+# -------------------------------------- #
 TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
@@ -38,8 +53,6 @@ TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-us'
 
 LANG='en_US.UTF-8'
-
-SITE_ID = 1
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -52,6 +65,15 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+# -------------------------------------- #
+# SITE ID
+# -------------------------------------- #
+SITE_ID = 1
+
+
+# -------------------------------------- #
+# UPLOADED MEDIA
+# -------------------------------------- #
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = ''
@@ -61,6 +83,10 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
+
+# -------------------------------------- #
+# STATIC MEDIA
+# -------------------------------------- #
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -71,15 +97,14 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, "static")
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = env('STATIC_URL', '/static/')
 
-# Admin media pulled from Django Grappelli
-# ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+if DEBUG:
+    STATIC_URL = '/static/'
+else:
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+# Admin media pulled from Django Grappelli
+# Waiting on 1.4 compatibility
+# ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -89,37 +114,29 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
 STATIC_S3_PATH = env('STATIC_S3_PATH', 'static')
 
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID', '')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY', '')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', '')
 
+
+# -------------------------------------- #
+# SECRET KEY
+# -------------------------------------- #
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = env('SECRET_KEY', 'secret')
 
+
+# -------------------------------------- #
+# TEMPLATES
+# -------------------------------------- #
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
 )
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-ROOT_URLCONF = 'proman.urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'proman.wsgi.application'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     "django.contrib.auth.context_processors.auth",
@@ -136,11 +153,27 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_ROOT, "templates"),
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, "pm", "templates"),
 )
 
+
+# -------------------------------------- #
+# MIDDLEWARE
+# -------------------------------------- #
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+
+# -------------------------------------- #
+# APPS
+# -------------------------------------- #
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -163,14 +196,29 @@ INSTALLED_APPS = (
 
 AUTH_PROFILE_MODULE = 'pm.Profile'
 
-SITE_CACHE_KEY = "proman"
 
+# -------------------------------------- #
+# HARVEST INTEGRATIONS
+# -------------------------------------- #
 HV_URL = env('HV_URL', '')
 HV_USER = env('HV_USER', '')
 HV_PASS = env('HV_PASS', '')
 
 
-LOCAL_CACHE_PATH = os.path.join(PROJECT_ROOT, "cache")
+# -------------------------------------- #
+# SUGARCRM INTEGRATIONS
+# -------------------------------------- #
+SC_URL = env('SC_URL', '')
+SC_USER = env('SC_USER', '')
+SC_PASS = env('SC_PASS', '')
+
+
+# -------------------------------------- #
+# CACHE
+# -------------------------------------- #
+SITE_CACHE_KEY = "proman"
+
+LOCAL_CACHE_PATH = env('LOCAL_CACHE_PATH', os.path.join(PROJECT_ROOT, "cache"))
 
 CACHES = {
     'default': {
@@ -192,31 +240,3 @@ if MEMCACHE_SERVERS:
         }
     }
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
