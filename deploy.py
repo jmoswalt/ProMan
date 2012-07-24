407b9d3a4445d2e0af597aadb2f0d19ad983db52
jmoswalt <jmoswalt@gmail.com>
2012-07-23 20:46:08 -0500

    Adding no input to deploy script

--- deploy.py
+++ deploy.py
@@ -1,5 +1,5 @@
 import os
 
 os.system('git push heroku master')
-os.system('heroku run:detached python manage.py collectstatic')
+os.system('heroku run:detached python manage.py collectstatic --noinput')
 os.system('heroku run python manage.py syncdb')
